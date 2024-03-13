de92fb10419a7e8429c73752945eb7073cea0c9a
vathuglife <thuandt582@gmail.com>
2024-03-13 10:08:41 +0700

    vathuglife: added more UI Components

--- ChatBot/MVVM/ViewModel/MainViewModel.cs
+++ ChatBot/MVVM/ViewModel/MainViewModel.cs
@@ -1,15 +1,22 @@
 ﻿using ChatBot_Repo.Core;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace ChatBot.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
         public ConversationViewModel ConversationVM { get; set; }
+        public ObservableCollection<RadioButton> Conversations { get; set; }
         private object _currentView;
 
         public object CurrentView
@@ -24,8 +31,39 @@ namespace ChatBot.MVVM.ViewModel
 
         public MainViewModel()
         {
+            InitializeObjects();
+            LoadConversations();
+        }
+        private void InitializeObjects()
+        {
+            Conversations = new ObservableCollection<RadioButton>();
             ConversationVM = new ConversationViewModel();
             CurrentView = ConversationVM;
         }
+        private void LoadConversations()
+        {
+            List<string> contents = new List<string>()
+            {
+                "Healthcare","Politics","Education","Science",
+            };
+            int count = 1;
+            foreach(string content in contents)
+            {                
+                Conversations.Add(new RadioButton()
+                {
+                    Name = $"ConversationItem{count}",
+                    Content = content,
+                    Height = 50,
+                    Foreground = new SolidColorBrush(Colors.White),
+                    FontSize = 14,
+                    Style = GetRadioButtonStyle(),                    
+                }) ;
+            }
+            
+        }       
+        private Style GetRadioButtonStyle()
+        {
+            return Application.Current.FindResource("ConversationItemTheme") as Style;
+        }       
     }
 }
