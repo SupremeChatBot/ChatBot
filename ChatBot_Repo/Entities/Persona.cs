511c5256d0d9ee42e84afcff8556749acb3b09f0
dawngn <haidangdavid123@gmail.com>
2024-03-20 08:52:25 +0700

    add:persona entity

---
+++ ChatBot_Repo/Entities/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot_Repo.Entities
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Prompt { get; set; }
+
+        public ICollection<Conversation> Conversations { get; set; }
+    }
+}
